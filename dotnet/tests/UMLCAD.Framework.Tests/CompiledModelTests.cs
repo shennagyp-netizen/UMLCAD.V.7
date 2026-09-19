85bcf10326a1e037b76eaceddb2f89cbf1c0510b
shennagyp-netizen <shennagyp@gmail.com>
2026-09-19 06:43:19 +0300

    test: require published face in compiled semantic graph

--- dotnet/tests/UMLCAD.Framework.Tests/CompiledModelTests.cs
+++ dotnet/tests/UMLCAD.Framework.Tests/CompiledModelTests.cs
@@ -97,6 +97,26 @@ public sealed class CompiledModelTests
     }
 
     [Fact]
+    public void Published_face_compiles_as_a_semantic_target_with_result_and_provenance_identity()
+    {
+        var builder = CadApplication.CreateBuilder();
+        builder.AddPart("part", "solid", part =>
+        {
+            part.TopologyBinding("topology-front", "face", "front-face", "result-r1", "brep-face-6");
+            part.Publication("front", "face", "front-face", "result-r1", "topology-front");
+        });
+
+        using var app = builder.Build();
+        var manifest = app.CreateCompiledModelManifest();
+
+        var face = manifest.Nodes.Single(x => x.Id == "definition:part:part/face:front-face");
+        Assert.Equal("Face", face.Kind);
+        Assert.Equal("result-r1", face.Metadata["resultIdentity"].GetString());
+        Assert.Equal("topology-front", face.Metadata["topologyBindingId"].GetString());
+        Assert.Equal("brep-face-6", face.Metadata["authoritativeTopologyId"].GetString());
+    }
+
+    [Fact]
     public void Occurrence_ids_may_repeat_in_different_assemblies_but_compiled_ids_are_unique()
     {
         var builder = CadApplication.CreateBuilder();

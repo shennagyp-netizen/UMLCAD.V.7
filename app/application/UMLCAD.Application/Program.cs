9daedd4aafcd7a386924520a6b5a3b52690269bd
shennagyp-netizen <shennagyp@gmail.com>
2026-09-19 19:04:32 +0300

    app: keep application host free of kernel transport

--- app/application/UMLCAD.Application/Program.cs
+++ app/application/UMLCAD.Application/Program.cs
@@ -1,14 +1,7 @@
 using UMLCAD.Framework;
-using UMLCAD.Kernel;
 
-using var httpClient = new HttpClient
-{
-    BaseAddress = new Uri("http://127.0.0.1:8080/")
-};
-
-var application = new UmlcadApplication(new UmlcadKernel(httpClient));
+using var application = UmlcadApplication.ConnectDefault();
 
 Console.WriteLine("UMLCAD.Application host");
 Console.WriteLine("Application Layer: app/framework/libraries");
 Console.WriteLine($"Application facade: {application.GetType().Name}");
-Console.WriteLine($"Kernel gateway: {typeof(UmlcadKernel).Assembly.GetName().Name}");

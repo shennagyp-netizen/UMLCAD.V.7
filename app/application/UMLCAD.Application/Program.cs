2d7ef6897a197d7b69c8a0a519ff86856a139752
shennagyp-netizen <shennagyp@gmail.com>
2026-09-19 18:50:08 +0300

    app: add first Application Layer vertical foundation

---
+++ app/application/UMLCAD.Application/Program.cs
@@ -0,0 +1,14 @@
+using System.Net.Http;
+using UMLCAD.Framework;
+using UMLCAD.Kernel;
+
+using var httpClient = new HttpClient
+{
+    BaseAddress = new Uri("http://127.0.0.1:8080/")
+};
+
+var application = new UmlcadApplication(new UmlcadKernel(httpClient));
+
+Console.WriteLine("UMLCAD.Application host");
+Console.WriteLine("Application Layer: app/framework/libraries");
+Console.WriteLine($"Kernel gateway: {typeof(UmlcadKernel).Assembly.GetName().Name}");

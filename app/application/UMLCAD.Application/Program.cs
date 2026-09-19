d88cdda8697bc4fbe83b4b05f9db553d9e1ba0a1
shennagyp-netizen <shennagyp@gmail.com>
2026-09-19 21:16:30 +0300

    refactor: propagate kernel history through app tests and demos

--- app/application/UMLCAD.Application/Program.cs
+++ app/application/UMLCAD.Application/Program.cs
@@ -16,6 +16,6 @@ sealed class DemoKernel:IKernelGateway
  {
    c.ThrowIfCancellationRequested();
    var id=r.OperationKind switch{"Cad.Sketch"=>"demo:sketch","Cad.Extrusion"=>"demo:body1","Cad.Hole"=>"demo:body2",_=>"demo:unknown"};
-   return Task.FromResult(KernelOperationResponse.Success(r,new CadResultId(id),"demo:"+r.EvaluationIdentity.Value,new[]{new KernelTopologyBinding("operation",r.OperationId.Value)}));
+   return Task.FromResult(KernelOperationResponse.Success(r,new CadResultId(id),"demo:"+r.EvaluationIdentity.Value,new KernelHistoryIdentity("demo-history:"+r.OperationId.Value),new[]{new KernelTopologyBinding("operation",r.OperationId.Value)}));
  }
 }

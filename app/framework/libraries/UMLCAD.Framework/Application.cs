22dd59afdfc6466f8160ec810bf8c56516039f13
shennagyp-netizen <shennagyp@gmail.com>
2026-09-19 19:04:30 +0300

    app: move kernel construction behind framework facade

--- app/framework/libraries/UMLCAD.Framework/Application.cs
+++ app/framework/libraries/UMLCAD.Framework/Application.cs
@@ -4,13 +4,41 @@ using UMLCAD.Kernel;
 
 namespace UMLCAD.Framework;
 
-public sealed class UmlcadApplication(
-    UmlcadKernel kernel)
+public sealed class UmlcadApplication : IDisposable
 {
-    private readonly CadEvaluationEngine _evaluation = new(kernel ?? throw new ArgumentNullException(nameof(kernel)));
+    private readonly UmlcadKernel _kernel;
+    private readonly CadEvaluationEngine _evaluation;
+    private bool _disposed;
+
+    private UmlcadApplication(UmlcadKernel kernel)
+    {
+        _kernel = kernel ?? throw new ArgumentNullException(nameof(kernel));
+        _evaluation = new CadEvaluationEngine(_kernel);
+    }
+
+    public static UmlcadApplication ConnectDefault(UmlcadKernelOptions? options = null) =>
+        new(UmlcadKernel.Connect(options));
 
     public Task<KernelEvaluationOutcome> BuildAsync(
         CadBuildDefinition definition,
-        CancellationToken cancellationToken = default) =>
-        _evaluation.EvaluateAsync(definition, cancellationToken);
+        CancellationToken cancellationToken = default)
+    {
+        ThrowIfDisposed();
+        return _evaluation.EvaluateAsync(definition, cancellationToken);
+    }
+
+    public void Dispose()
+    {
+        if (_disposed)
+            return;
+
+        _disposed = true;
+        _kernel.Dispose();
+    }
+
+    private void ThrowIfDisposed()
+    {
+        if (_disposed)
+            throw new ObjectDisposedException(nameof(UmlcadApplication));
+    }
 }
